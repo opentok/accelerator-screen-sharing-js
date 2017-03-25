580fbe0fd9921e8eccbba4dac20c5469f7b68efc
acn <acn@alberto.local>
2017-03-25 19:36:25 +0100

    Moving testrunner from grunt to npm

--- karma.conf.js
+++ karma.conf.js
@@ -1,20 +1,30 @@
 module.exports = function(config) {
   var customLaunchers = {
-      sl_chrome: {
-        base: 'SauceLabs',
-        browserName: 'chrome',
-        platform: 'OS X 10.11',
-        version: '56'
-      }
+    sl_chrome: {
+      base: 'SauceLabs',
+      browserName: 'chrome',
+      platform: 'OS X 10.11',
+      version: '56'
+    }
   };
 
   var configuration = {
+        plugins: [
+           'karma-mocha', 'karma-coverage', 'karma-html2js-preprocessor', 'karma-sauce-launcher', 'karma-chrome-launcher', 'karma-chai'
+        ],
         basePath: '',
-        frameworks: ['mocha'],
+        frameworks: ['mocha', 'chai'],
+        client: {
+            contextFile: '/test/index.html'
+        },
         files: [
           'https://static.opentok.com/v2/js/opentok.min.js',
           'https://code.jquery.com/jquery-1.10.2.js',
-          'https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min.js'
+          'https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min.js',
+          'node_modules/opentok-solutions-logging/src/opentok-solutions-logging.js',
+          'src/opentok-screen-sharing.js',
+          'test/components/accelerator-pack.js',
+          'test/opentok-screen-sharing-test.js'
         ],
         exclude: [
         ],
@@ -40,13 +50,16 @@ module.exports = function(config) {
                 { type: 'lcovonly', subdir: '.', file: 'report-lcovonly.txt' }
             ]
         },
-      sauceLabs: {
-        testName: 'Accelerator Screen Sharing Unit Tests',
-        username: process.env.SAUCE_USERNAME,
-        accessKey: process.env.SAUCE_ACCESS_KEY
-    },
-    customLaunchers: customLaunchers,
-    browsers: Object.keys(customLaunchers),
+        sauceLabs: {
+          testName: 'Accelerator Screen Share Unit Tests',
+          username: process.env.SAUCE_USERNAME,
+          accessKey: process.env.SAUCE_ACCESS_KEY
+        }
+    };
+
+    if (process.env.TRAVIS) {
+       configuration.customLaunchers = customLaunchers;
+       configuration.browsers = Object.keys(customLaunchers);
+    }
+    config.set(configuration);
   };
-  config.set(configuration);
-};
