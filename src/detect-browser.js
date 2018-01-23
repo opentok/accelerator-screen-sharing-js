7b06b5d2e56dfa306c7a644880afb1d78970e104
Mike Thomas <mthomas@tokbox.com>
2018-01-23 10:38:10 +0100

    Replace OT.$.browser() with pure js function

---
+++ src/detect-browser.js
@@ -0,0 +1,40 @@
+function detectBrowser() {
+
+  const navigator = window && window.navigator;
+
+  // Fail early if it's not a browser
+  if (typeof window === 'undefined' || !window.navigator) {
+    return 'not a browser';
+  }
+
+  // Firefox.
+  if (navigator.mozGetUserMedia) {
+    return 'Firefox';
+  }
+  if (navigator.webkitGetUserMedia) {
+    // Chrome, Chromium, Webview, Opera, all use the chrome shim for now
+    if (window.hasOwnProperty('webkitRTCPeerConnection')) {
+      return 'Chrome';
+    }
+    if (navigator.userAgent.match(/Version\/(\d+).(\d+)/)) {
+      return 'Safari';
+    }
+    return 'WebKit browser without WebRTC support';
+  }
+
+  if (navigator.mediaDevices && navigator.userAgent.match(/Edge\/(\d+).(\d+)$/)) { // Edge.
+    return 'Edge';
+  }
+
+  if (navigator.userAgent.indexOf('MSIE ') > 0 ||
+    !!navigator.userAgent.match(/Trident.*rv\:11\./)) {
+    return 'Internet Explorer';
+  }
+
+  if (navigator.mediaDevices && navigator.userAgent.match(/AppleWebKit\/(\d+)\./)) {
+    // Safari, with webkitGetUserMedia removed.
+    return 'Safari';
+  }
+  // Default fallthrough: not supported.
+  return 'unsupported browser';
+}
