996570371f303612b5a740de9afa42335a1e6737
Mike Thomas <mthomas@tokbox.com>
2018-01-23 10:52:36 +0100

    Add new file to tests

--- karma.conf.js
+++ karma.conf.js
@@ -22,7 +22,7 @@ module.exports = function(config) {
           'https://code.jquery.com/jquery-1.10.2.js',
           'https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min.js',
           'node_modules/opentok-solutions-logging/src/opentok-solutions-logging.js',
-          'src/opentok-screen-sharing.js',
+          'dist/opentok-screen-sharing.js',
           'test/components/accelerator-pack.js',
           'test/opentok-screen-sharing-test.js'
         ],
